5bff1112df257eb819659d0ea6a8c5e522f8f765
Alex <alex@olegs-macbook-pro.local>
2019-07-22 10:23:38 +0300

    Initial Commit

---
+++ com.inventorymaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  com.inventorymaster
+//
+//  Created by Alex on 7/22/19.
+//  Copyright © 2019 Alex Mytnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
